21c287d74638b94ef14c06e82e0a40c226bba75b
Almudena Lopez Rodriguez <almlopez@c2r16s2.42madrid.com>
2025-02-15 16:48:11 +0100

    consigo que funcione la d y la i

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: almlopez <almlopez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 11:29:26 by almudenalop       #+#    #+#             */
-/*   Updated: 2025/02/15 16:14:49 by almlopez         ###   ########.fr       */
+/*   Updated: 2025/02/15 16:46:37 by almlopez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ int	ft_putnbr_fd(int n, int fd)
 		src[i++] = (num % 10) + '0';
 		num = num / 10;
 	}
-	l *= i;
+	l += i;
 	while (--i >= 0)
 		write(fd, &src[i], 1);
 	return (l);
